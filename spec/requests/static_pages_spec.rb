6bae646b972db112aedead18ffaa9dfe0ca5eca3
Diarmuid King <diarmuid.king@gmail.com>
2013-07-12 09:33:23 -0400

    remove old index

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,43 +2,38 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+   subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "| Home")
-    end
+    it { should have_selector('h1', text: 'Welcome to the Sample App') }
+    it { should have_selector 'title',  text: "Ruby on Rails Tutorial Sample App" }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "| Help")
+      visit help_path
+      page.should have_selector('title',  :text => "| Help")
     end
   end
 
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',   :text => " | About Us")
     end
   end
@@ -46,7 +41,7 @@ describe "Static pages" do
 
 describe "Contact page" do
   it "should have h1 Contact Us" do
-    visit '/static_pages/contact'
+    visit contact_path
     page.should have_selector('h1', :text=>'Contact Us')
   end
 end
